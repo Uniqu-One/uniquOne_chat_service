973b0e7b266fad5d13f4267cb7a1a3dc5c93c12c
yeon <sy423513@gmail.com>
2022-11-04 18:05:48 +0900

    chatResponseDto 수정

--- src/main/java/com/sparos/uniquone/msachatservice/utils/ControllerExceptionHandler.java
+++ src/main/java/com/sparos/uniquone/msachatservice/utils/ControllerExceptionHandler.java
@@ -3,7 +3,6 @@ package com.sparos.uniquone.msachatservice.utils;
 import com.sparos.uniquone.msachatservice.utils.response.ExceptionResponse;
 import com.sparos.uniquone.msachatservice.utils.response.UniquOneServiceException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
